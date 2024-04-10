31e8c188e9749e09649f918adf180ec92d78333b
Thinh <nguyenhungthinh2504@gmail.com>
2024-04-10 16:30:46 +0700

    feat(FPD-2): fix bug

--- src/pages/Users/CreateUser/CreateUser.tsx
+++ src/pages/Users/CreateUser/CreateUser.tsx
@@ -1,20 +1,30 @@
 import CreateUserForm from "@/components/form/createUserForm";
-import ButtonBack from "@/components/layout/buttonBack/buttonBack";
+import { Breadcrumb } from "antd";
+import { useTranslation } from "react-i18next";
 import { Link } from "react-router-dom";
 
 import "./CreateUser.scss";
 
 const CreateUser = () => {
+  const { t } = useTranslation();
   return (
-    <div className='page-create-user'>
-      {/* Back Button */}
-      <Link to='../list'>
-        <ButtonBack />
-      </Link>
+    <div>
+      <div className='page-create-user'>
+        <Breadcrumb
+          items={[
+            {
+              title: <Link to='../list'>{t("MENU.LIST_USERS")}</Link>,
+            },
+            {
+              title: t("MENU.CREATE_USER"),
+            },
+          ]}
+        />
 
-      {/* Form */}
-      <div className='form-create'>
-        <CreateUserForm />
+        {/* Form */}
+        <div className='form-create'>
+          <CreateUserForm />
+        </div>
       </div>
     </div>
   );
