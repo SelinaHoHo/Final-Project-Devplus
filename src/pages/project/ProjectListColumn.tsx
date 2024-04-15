369ff401788530cb40cb6749bc36377b97b3379c
trandangquang <trandangquang2203@gmail.com>
2024-04-15 11:39:35 +0700

    feat(FPD-31): add row members

--- src/pages/project/ProjectListColumn.tsx
+++ src/pages/project/ProjectListColumn.tsx
@@ -1,8 +1,8 @@
 import { ButtonAction } from "@/components/core/ButtonAction/ButtonAction";
 import i18n from "@/config/i18n";
 import { ColumnIProject } from "@/interfaces/project/projects.interface";
-import { DeleteOutlined, EditOutlined } from "@ant-design/icons";
-import { Progress, Space, Tag } from "antd";
+import { DeleteOutlined, EditOutlined, FileSearchOutlined } from "@ant-design/icons";
+import { Avatar, Progress, Space, Tag } from "antd";
 import { ColumnsType } from "antd/lib/table";
 import { Translation } from "react-i18next";
 
@@ -24,12 +24,12 @@ export const ProjectsColumnsTable = (
   {
     title: <Translation>{(t) => t("PROJECT.STARTDATE")}</Translation>,
     dataIndex: "startDate",
-    width: "13%",
+    width: "15%",
   },
   {
     title: <Translation>{(t) => t("PROJECT.TARGETDATE")}</Translation>,
     dataIndex: "endDate",
-    width: "13%",
+    width: "15%",
   },
   {
     title: <Translation>{(t) => t("PROJECT.PROJECTSTATUS")}</Translation>,
@@ -46,7 +46,7 @@ export const ProjectsColumnsTable = (
     ],
     onFilter: (value, record) => record.status.indexOf(value as string) === 0,
     filterSearch: true,
-    width: "12%",
+    width: "10%",
     render: (status) => <Tag color={status === "Completed" ? "green" : "geekblue"}>{status}</Tag>,
   },
   {
@@ -55,6 +55,28 @@ export const ProjectsColumnsTable = (
     sorter: (a, b) => a.progress - b.progress,
     width: "20%",
     render: (_text, record) => <Progress percent={record.progress} />,
+  },
+  {
+    title: <Translation>{(t) => t("PROJECT.MEMBERS")}</Translation>,
+    dataIndex: "members",
+    width: "10%",
+    render: (_text) => (
+      <>
+        <Avatar.Group
+          maxCount={3}
+          size='large'
+          maxStyle={{
+            color: "#f56a00",
+            backgroundColor: "#fde3cf",
+          }}
+        >
+          <Avatar src='https://api.dicebear.com/7.x/miniavs/svg?seed=3' />
+          <Avatar src='https://api.dicebear.com/7.x/miniavs/svg?seed=3' />
+          <Avatar src='https://api.dicebear.com/7.x/miniavs/svg?seed=3' />
+          <Avatar src='https://api.dicebear.com/7.x/miniavs/svg?seed=3' />
+        </Avatar.Group>
+      </>
+    ),
   },
   {
     title: <Translation>{(t) => t("PROJECT.ACTIONS")}</Translation>,
@@ -69,6 +91,13 @@ export const ProjectsColumnsTable = (
           tooltip={i18n.t("ACTION.EDIT")}
         >
           <EditOutlined />
+        </ButtonAction>
+        <ButtonAction
+          variant='success'
+          handleAction={() => handleAction("detail", record)}
+          tooltip={i18n.t("ACTION.DETAIL")}
+        >
+          <FileSearchOutlined />
         </ButtonAction>
         <ButtonAction
           variant='danger'
