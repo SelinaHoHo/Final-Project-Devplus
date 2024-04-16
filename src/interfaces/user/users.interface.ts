cc91cc91795cfcb521b6f7182e8574a2889bcb55
KhanhNguyen2710 <hanaaimi304@gmail.com>
2024-04-16 11:20:39 +0700

    feat(FPD-4): add manager id

--- src/interfaces/user/users.interface.ts
+++ src/interfaces/user/users.interface.ts
@@ -1,25 +1,36 @@
 export interface IUser {
   id: string;
-  fullName: string;
-  gender: string;
   email: string;
-  positions: Position[];
-  status: string;
-  avatarUrl: string;
-  // isManager: boolean;
-  //
-  user: {
-    isManager: boolean;
-    managerId: string;
+  // userName: string;
+  isManager: boolean;
+  managerId: string | null;
+  manager?: {
+    userName: string;
+    profile: {
+      avatarUrl: string;
+      fullName: string;
+    };
+  };
+  profile: {
+    id: string;
     fullName: string;
+    phoneNumber: string | null;
+    email: string;
+    dayOfBirth: string;
+    avatarUrl: string;
+    gender: string;
+    status: string;
+    description: string;
+    positions: { id: string; name: string; description: string }[];
+    technicalMember: { id: string }[];
+    // positions: Position[];
   };
 }
-export interface Position {
-  id: string;
-  name: string;
-  description: string;
-}
-
+// export interface Position {
+//   id: string;
+//   name: string;
+//   description: string;
+// }
 export type IUsers = {
   data: IUser[];
   meta: {
@@ -27,7 +38,6 @@ export type IUsers = {
     pageCount: number;
   };
 };
-
 export type GetListUsers = {
   name: string;
   take: number;
