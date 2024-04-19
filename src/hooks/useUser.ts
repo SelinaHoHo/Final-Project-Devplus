06da2c0dba0e39f25bb1cecda3c2036674e9bd0c
trandangquang <52559661+trandangquang@users.noreply.github.com>
2024-04-19 13:19:49 +0700

    feat(FPD-23): delete user (#13)

--- src/hooks/useUser.ts
+++ src/hooks/useUser.ts
@@ -1,7 +1,9 @@
-import { getAllUserNoPagination, getUsers } from "@/apis/user.api";
+import { deleteUser, getAllUserNoPagination, getUsers } from "@/apis/user.api";
 import { QUERY_KEY } from "@/constants/queryKey";
-import { GetListUsers, IGetUsers, IUsers } from "@/interfaces/user/users.interface";
-import { UseQueryResult, useQuery } from "@tanstack/react-query";
+import { DeleteUser, GetListUsers, IGetUsers, IUsers } from "@/interfaces/user/users.interface";
+import { UseQueryResult, useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { notification } from "antd";
+import { t } from "i18next";
 
 export const useGetAccounts = (param: GetListUsers): UseQueryResult<IUsers, Error> => {
   return useQuery<IUsers>({
@@ -19,6 +21,30 @@ export const useGetAllUserNoPagination = (): UseQueryResult<IGetUsers, Error> =>
     queryFn: async (): Promise<IGetUsers> => {
       const { data } = await getAllUserNoPagination();
       return data;
+    },
+  });
+};
+
+export const useDeleteUser = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: async (param: DeleteUser) => {
+      const data = await deleteUser(param);
+      return data;
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: [QUERY_KEY.USERS],
+      });
+      notification.success({
+        message: t("DELETE_USER.SUCCESS") as string,
+      });
+    },
+    onError: () => {
+      notification.error({
+        message: t("DELETE_USER.FAILED") as string,
+        description: t("DELETE_USER.FAILED_MESSAGE") as string,
+      });
     },
   });
 };
