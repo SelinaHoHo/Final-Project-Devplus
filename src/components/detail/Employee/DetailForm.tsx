9160cac3b919d7fde8f356c28b2abd663de5e9cb
Thinh <nguyenhungthinh2504@gmail.com>
2024-04-25 15:26:29 +0700

    feat(FPD-68): fix UI create & detail Employee

--- src/components/detail/Employee/DetailForm.tsx
+++ src/components/detail/Employee/DetailForm.tsx
@@ -1,6 +1,6 @@
 import { IUserDetail } from "@/interfaces/user/users.interface";
 import { RootState } from "@/redux/store";
-import { Avatar, Card, Col, Row, Space, Steps, Table, Typography } from "antd";
+import { Avatar, Card, Col, Row, Steps, Table, Typography } from "antd";
 import moment from "moment";
 import { FC } from "react";
 import { useTranslation } from "react-i18next";
@@ -80,158 +80,6 @@ const DetailForm: FC<DataProps> = ({ data }) => {
     <div>
       {theme === "dark" ? (
         <div className='detail-employee-dark'>
-          {/* <Row gutter={[8, 8]} style={{ width: "100%" }}>
-            <Col md={8} lg={{ span: 24, flex: "column" }} style={{ width: "100%" }}>
-              <Col span={24}>
-                <Col span={12}>
-                  <Card bordered type='inner' style={{ padding: "20px" }}>
-                    <Row gutter={[8, 8]}>
-                      <Col span={24}>
-                        <Title level={2}>{t("DETAIL_EMPLOYEE.PROFILE")}</Title>
-                      </Col>
-                      <Col span={24}>
-                        <Row gutter={[24, 24]}>
-                          <Col span={12} style={{ display: "flex", alignItems: "center" }}>
-                            <Col span={12}>
-                              <Avatar size={450} src={data?.profile.avatarUrl} />
-                            </Col>
-                          </Col>
-
-                          <Col span={12}>
-                            <Col>
-                              <Text
-                                style={{
-                                  color: "#16c2c2",
-                                  fontSize: "30px",
-                                  margin: "0",
-                                }}
-                              >
-                                {data?.profile.fullName}
-                              </Text>
-                            </Col>
-
-                            <Col>
-                              <Text strong>
-                                {data?.projectHistory.map((day) =>
-                                  moment(day.createDate).format("MM/YYYY"),
-                                )}{" "}
-                                - {t("DETAIL_EMPLOYEE.NOW")}
-                              </Text>
-                            </Col>
-
-                            <Col>
-                              <Text style={{ fontSize: "16px", margin: "0", paddingTop: "10px" }}>
-                                <Text strong>{t("DETAIL_EMPLOYEE.ADDRESS")}:</Text>{" "}
-                                {data?.profile.address}
-                              </Text>
-                            </Col>
-
-                            <Col>
-                              <Text style={{ fontSize: "16px", margin: "0" }}>
-                                <Text strong>{t("DETAIL_EMPLOYEE.EMAIL")}:</Text> {data?.email}
-                              </Text>
-                            </Col>
-
-                            <Col>
-                              <Text style={{ fontSize: "16px" }}>
-                                <Text strong style={{ fontSize: "18px" }}>
-                                  {t("DETAIL_EMPLOYEE.POSITION")}:{" "}
-                                </Text>
-                                {data?.positionMember.map((item) => item?.postion.name).join(", ")}
-                              </Text>
-                            </Col>
-
-                            <Col span={24}>
-                              <Table
-                                dataSource={data?.languageMember}
-                                columns={columnsLanguage}
-                                pagination={false}
-                                style={{ margin: "0" }}
-                              />
-                            </Col>
-
-                            <Col>
-                              <Table
-                                dataSource={data?.technicalMember}
-                                columns={columnsTechnical}
-                                pagination={false}
-                                style={{ margin: "0" }}
-                              />
-                            </Col>
-
-                            <Col>
-                              <Text>
-                                {" "}
-                                <Text strong style={{ fontSize: "18px" }}>
-                                  {t("DETAIL_EMPLOYEE.DESCRIPTION")}:{" "}
-                                </Text>
-                                {data?.profile?.description}
-                              </Text>
-                            </Col>
-                          </Col>
-                        </Row>
-                      </Col>
-                    </Row>
-                  </Card>
-                </Col>
-
-                {data?.projectMembers?.map((item, index) => (
-                  <Card bordered type='inner' style={{ padding: "20px" }}>
-                    <Col key={index} span={24}>
-                      <Title level={3}>
-                        {item?.project.name} ({moment(item?.project.startDate).format("MM/YYYY")} -{" "}
-                        {moment(item?.project.endDate).format("MM/YYYY")})
-                      </Title>
-                      <br />
-
-                      <Text style={{ fontSize: "16px" }}>
-                        <Text strong style={{ fontSize: "18px" }}>
-                          {t("DETAIL_EMPLOYEE.ROLE")}:{" "}
-                        </Text>
-                        {item?.roles?.length
-                          ? item.roles.map((role) => role?.position?.name).join(", ")
-                          : "Manager"}
-                      </Text>
-                      <br />
-
-                      <Text style={{ fontSize: "16px" }}>
-                        <Text strong style={{ fontSize: "18px" }}>
-                          {t("DETAIL_EMPLOYEE.LANGUAGE_FRAMEWORK")}:
-                        </Text>{" "}
-                        {item?.project?.languageProject
-                          ?.map((lang) => lang?.language?.name)
-                          .join(", ")}
-                      </Text>
-                      <br />
-
-                      <Text style={{ fontSize: "16px" }}>
-                        <Text strong style={{ fontSize: "18px" }}>
-                          {t("DETAIL_EMPLOYEE.TECHNICAL")}:
-                        </Text>{" "}
-                        {item?.project?.technicalProject
-                          ?.map((tech) => tech?.technical?.name)
-                          .join(", ")}
-                      </Text>
-                      <br />
-
-                      <Text style={{ fontSize: "16px" }}>
-                        <Text strong style={{ fontSize: "18px" }}>
-                          {t("DETAIL_EMPLOYEE.DESCRIPTION")}:
-                        </Text>{" "}
-                        {item?.project.description}
-                      </Text>
-                      <br />
-                      <hr />
-                    </Col>
-                  </Card>
-                ))}
-              </Col>
-            </Col>
-
-            <Col span={24}>
-              <Card>a</Card>
-            </Col>
-          </Row> */}
           <Row gutter={[8, 8]}>
             <Col span={12}>
               <Col span={24}>
@@ -393,56 +241,46 @@ const DetailForm: FC<DataProps> = ({ data }) => {
         </div>
       ) : (
         <div className='detail-employee-light'>
-          <Row gutter={[16, 16]} style={{ width: "100%" }}>
-            <Col md={24} lg={{ span: 24, flex: "column" }} style={{ width: "100%" }}>
-              <Space direction='vertical'>
-                <Card bordered type='inner' style={{ padding: "20px" }}>
+          <Row gutter={[8, 8]}>
+            <Col span={12}>
+              <Col span={24}>
+                <Card bordered type='inner' style={{ padding: "10px" }}>
                   <Row gutter={[8, 8]}>
-                    <Col span={24}>
+                    <Col span={24} style={{ display: "flex", justifyContent: "center" }}>
                       <Title level={2}>{t("DETAIL_EMPLOYEE.PROFILE")}</Title>
                     </Col>
+
                     <Col span={24}>
                       <Row gutter={[24, 24]}>
-                        <Col span={12} style={{ display: "flex", alignItems: "center" }}>
-                          <Col
-                            span={24}
-                            style={{
-                              display: "flex",
-                              alignItems: "center",
-                              justifyContent: "center",
-                            }}
-                          >
-                            <Avatar
-                              size={300}
-                              src={data?.profile.avatarUrl}
-                              style={{
-                                display: "flex",
-                                alignItems: "center",
-                                justifyContent: "center",
-                              }}
-                            />
-                          </Col>
+                        <Col span={24} style={{ display: "flex", justifyContent: "center" }}>
+                          <Avatar size={200} src={data?.profile.avatarUrl} />
                         </Col>
 
-                        <Col span={12}>
+                        <Col span={24}>
                           <Col>
                             <Text
                               style={{
                                 color: "#16c2c2",
                                 fontSize: "30px",
                                 margin: "0",
+                                display: "flex",
+                                justifyContent: "center",
                               }}
                             >
                               {data?.profile.fullName}
                             </Text>
                           </Col>
 
-                          <Col>
+                          <Col
+                            style={{
+                              display: "flex",
+                              justifyContent: "center",
+                              paddingBottom: "30px",
+                            }}
+                          >
                             <Text strong>
-                              {data?.projectHistory.map((day) =>
-                                moment(day.createDate).format("MM/YYYY"),
-                              )}{" "}
-                              - {t("DETAIL_EMPLOYEE.NOW")}
+                              {moment(data?.created_at).format("MM/YYYY")} -{" "}
+                              {t("DETAIL_EMPLOYEE.NOW")}
                             </Text>
                           </Col>
 
@@ -461,14 +299,12 @@ const DetailForm: FC<DataProps> = ({ data }) => {
 
                           <Col>
                             <Text style={{ fontSize: "16px" }}>
-                              <Text strong style={{ fontSize: "18px" }}>
-                                {t("DETAIL_EMPLOYEE.POSITION")}:{" "}
-                              </Text>
+                              <Text strong>{t("DETAIL_EMPLOYEE.POSITION")}: </Text>
                               {data?.positionMember.map((item) => item?.postion.name).join(", ")}
                             </Text>
                           </Col>
 
-                          <Col span={24}>
+                          <Col span={24} style={{ paddingTop: "10px" }}>
                             <Table
                               dataSource={data?.languageMember}
                               columns={columnsLanguage}
@@ -477,7 +313,7 @@ const DetailForm: FC<DataProps> = ({ data }) => {
                             />
                           </Col>
 
-                          <Col>
+                          <Col span={24} style={{ paddingTop: "10px" }}>
                             <Table
                               dataSource={data?.technicalMember}
                               columns={columnsTechnical}
@@ -486,10 +322,9 @@ const DetailForm: FC<DataProps> = ({ data }) => {
                             />
                           </Col>
 
-                          <Col>
+                          <Col style={{ paddingTop: "20px" }}>
                             <Text>
-                              {" "}
-                              <Text strong style={{ fontSize: "18px" }}>
+                              <Text strong style={{ fontSize: "16px" }}>
                                 {t("DETAIL_EMPLOYEE.DESCRIPTION")}:{" "}
                               </Text>
                               {data?.profile?.description}
@@ -500,58 +335,68 @@ const DetailForm: FC<DataProps> = ({ data }) => {
                     </Col>
                   </Row>
                 </Card>
+              </Col>
+            </Col>
 
-                {data?.projectMembers?.map((item, index) => (
-                  <Card bordered type='inner' style={{ padding: "20px" }}>
-                    <Col key={index} span={24}>
-                      <Title level={3}>
-                        {item?.project.name} ({moment(item?.project.startDate).format("MM/YYYY")} -{" "}
-                        {moment(item?.project.endDate).format("MM/YYYY")})
-                      </Title>
-                      <br />
+            <Col span={12}>
+              {data?.projectMembers?.map((item, index) => (
+                <Card bordered type='inner' style={{ padding: "20px" }}>
+                  <Col key={index} span={24}>
+                    <Title level={1} style={{ display: "flex", justifyContent: "center" }}>
+                      {item?.project.name}
+                    </Title>
+                    <br />
 
-                      <Text style={{ fontSize: "16px" }}>
-                        <Text strong style={{ fontSize: "18px" }}>
-                          {t("DETAIL_EMPLOYEE.ROLE")}:{" "}
-                        </Text>
-                        {item?.roles?.length
-                          ? item.roles.map((role) => role?.position?.name).join(", ")
-                          : "Manager"}
+                    <Steps direction='horizontal' size='small'>
+                      <Step title={moment(item?.project.startDate).format("DD/MM/YYYY")} />
+                      <Step
+                        title={moment(item?.project.endDate).format("DD/MM/YYYY")}
+                        status='finish'
+                      />
+                    </Steps>
+                    <br />
+
+                    <Text style={{ fontSize: "16px" }}>
+                      <Text strong style={{ fontSize: "18px" }}>
+                        {t("DETAIL_EMPLOYEE.ROLE")}:{" "}
                       </Text>
-                      <br />
+                      {item?.roles?.length
+                        ? item.roles.map((role) => role?.position?.name).join(", ")
+                        : "Manager"}
+                    </Text>
+                    <br />
 
-                      <Text style={{ fontSize: "16px" }}>
-                        <Text strong style={{ fontSize: "18px" }}>
-                          {t("DETAIL_EMPLOYEE.LANGUAGE_FRAMEWORK")}:
-                        </Text>{" "}
-                        {item?.project?.languageProject
-                          ?.map((lang) => lang?.language?.name)
-                          .join(", ")}
-                      </Text>
-                      <br />
+                    <Text style={{ fontSize: "16px" }}>
+                      <Text strong style={{ fontSize: "18px" }}>
+                        {t("DETAIL_EMPLOYEE.LANGUAGE_FRAMEWORK")}:
+                      </Text>{" "}
+                      {item?.project?.languageProject
+                        ?.map((lang) => lang?.language?.name)
+                        .join(", ")}
+                    </Text>
+                    <br />
 
-                      <Text style={{ fontSize: "16px" }}>
-                        <Text strong style={{ fontSize: "18px" }}>
-                          {t("DETAIL_EMPLOYEE.TECHNICAL")}:
-                        </Text>{" "}
-                        {item?.project?.technicalProject
-                          ?.map((tech) => tech?.technical?.name)
-                          .join(", ")}
-                      </Text>
-                      <br />
+                    <Text style={{ fontSize: "16px" }}>
+                      <Text strong style={{ fontSize: "18px" }}>
+                        {t("DETAIL_EMPLOYEE.TECHNICAL")}:
+                      </Text>{" "}
+                      {item?.project?.technicalProject
+                        ?.map((tech) => tech?.technical?.name)
+                        .join(", ")}
+                    </Text>
+                    <br />
 
-                      <Text style={{ fontSize: "16px" }}>
-                        <Text strong style={{ fontSize: "18px" }}>
-                          {t("DETAIL_EMPLOYEE.DESCRIPTION")}:
-                        </Text>{" "}
-                        {item?.project.description}
-                      </Text>
-                      <br />
-                      <hr />
-                    </Col>
-                  </Card>
-                ))}
-              </Space>
+                    <Text style={{ fontSize: "16px" }}>
+                      <Text strong style={{ fontSize: "18px" }}>
+                        {t("DETAIL_EMPLOYEE.DESCRIPTION")}:
+                      </Text>{" "}
+                      {item?.project.description}
+                    </Text>
+                    <br />
+                    <hr />
+                  </Col>
+                </Card>
+              ))}
             </Col>
           </Row>
         </div>
